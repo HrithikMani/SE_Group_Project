c5bc77c735bbea2ecfeacbfad70d6e9638d15941
sahil_ravuri <30614067+sahil-ravuri@users.noreply.github.com>
2024-11-17 09:55:39 -0500

    Update Modal UnassignedTasksModal.jsx

--- frontend/src/components/UnassignedTasksModal.jsx
+++ frontend/src/components/UnassignedTasksModal.jsx
@@ -1,5 +1,12 @@
 // UnassignedTasksModal.jsx
 import React from 'react';
 import Modal from 'react-modal';
+const UnassignedTasksModal = ({ isOpen, closeModal, unassignedOrders }) => {
+  return (
+    <Modal contentLabel="Unassigned Tasks" >
+      <h2>Unassigned Tasks</h2>
+    </Modal>
+  );
+};
 
 export default UnassignedTasksModal;
