736b04fa22f88d20ac07d95bb726fa18ba3ba976
Harshitha Enumula <142941181+harshitha-enumula@users.noreply.github.com>
2024-11-17 10:22:25 -0500

    Close modal on request.

--- frontend/src/components/UnassignedTasksModal.jsx
+++ frontend/src/components/UnassignedTasksModal.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Modal from 'react-modal';
 const UnassignedTasksModal = ({ isOpen, closeModal, unassignedOrders }) => {
   return (
-    <Modal style={{alinItems: 'center'}} isOpen={isOpen} contentLabel="Unassigned Tasks" >
-      <h2>Unassigned Tasks</h2>
+    <Modal style={{alinItems: 'center'}} isOpen={isOpen}  onRequestClose={closeModal} contentLabel="Unassigned Tasks" >
+      <h2 style={{paddingTop: '100px'}} >Unassigned Tasks</h2>
       <ul>
       {unassignedOrders === 0 ?(<li>No tasks found</li>) : (unassignedOrders.map((order)=> (
           <li key={order._id}>
@@ -12,6 +12,7 @@ const UnassignedTasksModal = ({ isOpen, closeModal, unassignedOrders }) => {
           </li>
         )))}
       </ul>
+       <button onClick={closeModal}>Close</button>
     </Modal>
   );
 };
