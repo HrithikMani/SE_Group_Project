2fe0a3047e725d1a456dc95ca491f0b7da07a0be
sahil_ravuri <30614067+sahil-ravuri@users.noreply.github.com>
2024-11-17 10:24:47 -0500

    Handle empty unassigned orders list

--- frontend/src/components/UnassignedTasksModal.jsx
+++ frontend/src/components/UnassignedTasksModal.jsx
@@ -6,11 +6,7 @@ const UnassignedTasksModal = ({ isOpen, closeModal, unassignedOrders }) => {
     <Modal style={{alinItems: 'center'}} isOpen={isOpen}  onRequestClose={closeModal} contentLabel="Unassigned Tasks" >
       <h2 style={{paddingTop: '100px'}} >Unassigned Tasks</h2>
       <ul>
-      {unassignedOrders === 0 ?(<li>No tasks found</li>) : (unassignedOrders.map((order)=> (
-          <li key={order._id}>
-            {order.trainNo} - {order.compartment} - {order.location} - {order.issue}
-          </li>
-        )))}
+      {unassignedOrders === 0 ?(<li>No tasks found</li>) : (unassignedOrders.map((order)=> (<li key={order._id}> {order.trainNo} - {order.compartment} - {order.location} - {order.issue} </li> )))}
       </ul>
        <button onClick={closeModal}>Close</button>
     </Modal>
